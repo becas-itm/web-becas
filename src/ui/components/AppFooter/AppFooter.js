71b3eaba68e84b48a063d3390dd564560e2afbd8
salchichongallo <salchichongallo1997@gmail.com>
2020-02-25 07:12:34 -0500

    feat(ui): add AppFooter component

---
+++ src/ui/components/AppFooter/AppFooter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AppFooter.scss';
+
+export function AppFooter() {
+  return (
+    <footer className="AppFooter">
+      <ul>
+        <li>© 2020 ITM</li>
+        <li>・</li>
+        <li>Términos</li>
+        <li>・</li>
+        <li>Privacidad</li>
+      </ul>
+    </footer>
+  );
+}
