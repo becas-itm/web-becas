41698bb2166b858a001fe8ab98823550f1f8d89c
salchichongallo <salchichongallo1997@gmail.com>
2020-02-25 13:46:37 -0500

    feat(ui): add Checkbox component

---
+++ src/ui/components/Checkbox/Checkbox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import './Checkbox.scss';
+
+export function Checkbox({ className, children, ...restProps }) {
+  return (
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
+    <label className={`Checkbox ${className || ''}`}>
+      <input
+        {...restProps}
+        type="checkbox"
+        className="Checkbox__input a-visuallyHidden"
+      />
+      <div className="Checkbox__checkMark" />
+      {children && <span className="Checkbox__label">{children}</span>}
+    </label>
+  );
+}
+
+Checkbox.propTypes = {
+  children: propTypes.node,
+  className: propTypes.string,
+};
