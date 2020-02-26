7ac18ed03401aa73ab056c44345913fc54c6bada
salchichongallo <salchichongallo1997@gmail.com>
2020-02-26 09:13:45 -0500

    refactor(ThreeRowTemplate): pass rest props

--- src/ui/templates/ThreeRowTemplate/ThreeRowTemplate.js
+++ src/ui/templates/ThreeRowTemplate/ThreeRowTemplate.js
@@ -3,11 +3,11 @@ import propTypes from 'prop-types';
 import { AppFooter } from 'ui/components/AppFooter';
 import './ThreeRowTemplate.scss';
 
-export function ThreeRowTemplate({ header, children }) {
+export function ThreeRowTemplate({ header, className = '', ...restProps }) {
   return (
     <div className="ThreeRowTemplate">
       <div>{header}</div>
-      <div className="ThreeRowTemplate__main">{children}</div>
+      <div {...restProps} className={`ThreeRowTemplate__main ${className}`} />
       <div>
         <AppFooter />
       </div>
