46bbe2d503b00cac1d4c22c6607c812e98077673
salchichongallo <salchichongallo1997@gmail.com>
2020-02-26 09:13:18 -0500

    feat(web): init HomePage page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AppLogo } from 'ui/components';
-import './App.css';
 
-function HomePage() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AppLogo className="App-logo" />
-        <p>Becas y convocatorias</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import HomePage from 'pages/HomePage';
 
 function App() {
   return (
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SiteTemplate } from 'ui/templates/SiteTemplate';
+
+export default function HomePage() {
+  return (
+    <SiteTemplate>
+      <p>Becas y convocatorias</p>
+    </SiteTemplate>
+  );
+}
