7cb0ef22950af057dc17faa6cd5b5c338052f624
salchichongallo <salchichongallo1997@gmail.com>
2020-02-29 22:36:38 -0500

    chore(web): proxy API requests

---
+++ src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: process.env.API_PROXY || 'http://127.0.0.1:3000/api/',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/',
+      },
+    }),
+  );
+};
