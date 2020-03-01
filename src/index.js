2d4f98901eac4960727742c8a51a77494e4f4a03
salchichongallo <salchichongallo1997@gmail.com>
2020-02-29 22:42:14 -0500

    feat(web): add splash screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import './styles.css';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SplashScreen from 'ui/components/SplashScreen';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = lazy(() => import('./App'));
+
+ReactDOM.render(
+  <Suspense fallback={<SplashScreen />}>
+    <App />
+  </Suspense>,
+  document.getElementById('root'),
+);
