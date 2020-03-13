2f816913d5cb4edede4f348014b0e2c6b975dd03
salchichongallo <salchichongallo1997@gmail.com>
2020-03-13 09:39:58 -0500

    refactor(Listbox): re-export ListboxPopover component

--- src/ui/components/Listbox/ListboxPopover.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ListboxPopover as BaseListboxPopover } from '@reach/listbox';
-
-export function ListboxPopover(props) {
-  return <BaseListboxPopover portal={false} {...props} />;
-}
-
-ListboxPopover.propTypes = BaseListboxPopover.propTypes;
--- src/ui/components/Listbox/index.js
+++ src/ui/components/Listbox/index.js
@@ -1,9 +1,12 @@
-export { ListboxOption, ListboxInput, ListboxList } from '@reach/listbox';
+export {
+  ListboxOption,
+  ListboxInput,
+  ListboxList,
+  ListboxPopover,
+} from '@reach/listbox';
 
 export { Listbox } from './Listbox';
 
 export { ListboxButton } from './ListboxButton';
 
 export { ListboxArrow } from './ListboxArrow';
-
-export { ListboxPopover } from './ListboxPopover';
