cee536d2cbfa05f877bfb0b631b7b04bdf37a139
salchichongallo <salchichongallo1997@gmail.com>
2020-03-15 17:01:55 -0500

    feat(ScholarshipPage): add scholarship details steps

--- src/pages/ScholarshipPage.js
+++ src/pages/ScholarshipPage.js
@@ -3,14 +3,17 @@ import useFetch from 'use-http';
 import { format } from 'date-fns';
 import locale from 'date-fns/locale/es';
 import { useParams } from 'react-router';
+import { DialogOverlay, DialogContent } from '@reach/dialog';
 
+import { useToggle } from 'utils/hooks';
 import Spinner from 'ui/components/Spinner';
 import CountryFlag from 'ui/components/CountryFlag';
+import { Button, KIND } from 'ui/components/Button';
 import EntityAvatar from 'ui/components/EntityAvatar';
 import { LinkButton } from 'ui/components/LinkButton';
 import NotFoundGhost from 'ui/components/NotFoundGhost';
 import { SiteTemplate } from 'ui/templates/SiteTemplate';
-import { Event, School, Money } from 'ui/components/Icon';
+import { Event, School, Money, OpenInNew } from 'ui/components/Icon';
 
 function formatDeadline(date) {
   const FORMAT = `d 'de' MMMM 'de' yyyy`;
@@ -41,6 +44,106 @@ function getAcademicLevel(level) {
     default:
       return 'Otros';
   }
+}
+
+function IcetexDialogDetails({ id }) {
+  const [detailsOpened, toggleDetails] = useToggle();
+  const okButtonRef = React.useRef();
+
+  return (
+    <>
+      <div className="flex justify-center mt-2">
+        <Button onClick={toggleDetails} kind={KIND.tertiary}>
+          Ver detalles
+        </Button>
+      </div>
+      <DialogOverlay
+        isOpen={detailsOpened}
+        onClick={toggleDetails}
+        onDismiss={toggleDetails}
+        className="fixed inset-0 overflow-auto p-4"
+        style={{ background: 'hsla(0, 0%, 0%, .5)' }}
+        initialFocusRef={okButtonRef}
+      >
+        <DialogContent
+          aria-labelledby="scholarship-instructions"
+          className="pt-6 pb-3 mx-auto max-w-md rounded bg-white"
+          style={{
+            marginTop: '15vh',
+            boxShadow:
+              '0 12px 17px 2px rgba(0, 0, 0, .14), 0 5px 22px 4px rgba(0, 0, 0, .12), 0 7px 8px -4px rgba(0, 0, 0, .2)',
+          }}
+        >
+          <h3
+            id="scholarship-instructions"
+            className="text-xl px-6 pb-3 text-base font-semibold"
+          >
+            Detalle de convocatoria
+          </h3>
+          <p className="px-6 pb-3 text-sm">
+            Realiza los siguientes pasos para obtener más información acerca de
+            la convocatoria:
+          </p>
+          <ol className="list-decimal list-inside px-8 mb-1 text-sm text-gray-700">
+            <li>
+              Ingresa el portal de becas del{' '}
+              <a
+                className="hover:underline focus:underline text-primary"
+                href="https://portal.icetex.gov.co/Portal/Home/HomeEstudiante/becas/becas-para-estudios-en-el-exterior/becas-vigentes"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ICETEX
+                <OpenInNew auto className="inline-block ml-1 w-3 h-3" />
+              </a>
+            </li>
+            <li>
+              Selecciona la opción{' '}
+              <span className="italic">Número de Convocatoria</span>
+            </li>
+            <li>
+              Ingresa el siguiente número:{' '}
+              <span className="font-semibold">{id}</span>
+            </li>
+            <li>
+              Presiona en <span className="italic">Buscar</span>
+            </li>
+          </ol>
+          <div className="flex justify-end px-6">
+            <Button
+              onClick={toggleDetails}
+              ref={okButtonRef}
+              kind={KIND.tertiary}
+            >
+              Listo
+            </Button>
+          </div>
+        </DialogContent>
+      </DialogOverlay>
+    </>
+  );
+}
+
+function ScholarshipDetails({ spider, id, url }) {
+  if (spider === 'icetex') {
+    return <IcetexDialogDetails id={id} url={url} />;
+  }
+
+  return (
+    <div className="flex justify-center mt-2">
+      <Button
+        renderAs={'a'}
+        kind={KIND.tertiary}
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className=""
+      >
+        Ver detalles
+        <OpenInNew auto className="inline-block ml-1 w-3 h-3" />
+      </Button>
+    </div>
+  );
 }
 
 function ScholarshipPage() {
@@ -143,6 +246,11 @@ function ScholarshipPage() {
               </div>
             </div>
           </div>
+
+          <ScholarshipDetails
+            {...data.sourceDetails}
+            spider={data.spider.name}
+          />
         </div>
       </main>
     </SiteTemplate>
