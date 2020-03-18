35fab8d0029e25d02168c8ba98e6d3fe56519c1a
salchichongallo <salchichongallo1997@gmail.com>
2020-03-18 11:48:52 -0500

    feat(Spinner): add white prop

--- src/ui/components/Spinner/Spinner.js
+++ src/ui/components/Spinner/Spinner.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import classNames from 'classnames';
 import './Spinner.css';
 
-function Spinner({ size, className, style, ...restProps }) {
+function Spinner({ size, className, white, style, ...restProps }) {
+  const styles = classNames(
+    'Spinner relative inline-block',
+    {
+      'Spinner--color-white': white,
+    },
+    className,
+  );
   return (
     <div
       {...restProps}
       style={{ ...style, width: size, height: size }}
-      className={`Spinner relative inline-block ${className}`}
+      className={styles}
     />
   );
 }
