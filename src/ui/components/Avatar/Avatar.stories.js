8dc5086cc66844522df1642f323896b0ce148a51
salchichongallo <salchichongallo1997@gmail.com>
2020-04-07 13:49:02 -0500

    feat(Avatar): add story

---
+++ src/ui/components/Avatar/Avatar.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
+import Avatar, { SIZE } from './index';
+
+const RANDOM_IMAGE_URL = 'https://source.unsplash.com/128x128';
+
+export default {
+  title: 'Avatar',
+  component: Avatar,
+  decorators: [
+    withKnobs,
+    storyFn => (
+      <div className="h-screen flex items-center justify-center p-4">
+        {storyFn()}
+      </div>
+    ),
+  ],
+};
+
+export const sizes = () =>
+  Object.values(SIZE).map(size => (
+    <Avatar size={size} src={RANDOM_IMAGE_URL} key={size} />
+  ));
+
+export const customSize = () => (
+  <Avatar size={number('Size', 200)} src={RANDOM_IMAGE_URL} />
+);
+
+export const customImage = () => (
+  <Avatar src={text('Source', RANDOM_IMAGE_URL)} size={SIZE.extraLarge} />
+);
