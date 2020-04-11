479a95b4e332c2d6302dd727028f862d6b0d7ff9
salchichongallo <salchichongallo1997@gmail.com>
2020-04-11 10:55:07 -0500

    fix(Textarea): receive forwarded ref

--- src/ui/components/Textarea.js
+++ src/ui/components/Textarea.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Textarea = React.forwardRef(({ className, ...restProps }) => {
+const Textarea = React.forwardRef(({ className, ...restProps }, ref) => {
   const styles = classNames(
     'w-full px-3 py-2 rounded',
     restProps.readOnly || restProps.disabled ? 'bg-gray-300' : 'bg-gray-200',
     restProps.disabled && 'cursor-not-allowed',
   );
-  return <textarea {...restProps} className={styles} />;
+  return <textarea {...restProps} ref={ref} className={styles} />;
 });
 
 export default Textarea;
