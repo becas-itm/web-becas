f7747b72ab676ed5f21933f27d2e8258828c8b6e
salchichongallo <salchichongallo1997@gmail.com>
2020-04-11 10:39:49 -0500

    test(Icon): uncomment test cases

--- src/ui/components/Icon/Icon.test.js
+++ src/ui/components/Icon/Icon.test.js
@@ -18,53 +18,34 @@ describe('Icon component', () => {
     expect(getByTestId('icon-content')).toBeInTheDocument();
   });
 
-  // describe('icon size', () => {
-  //   const REGULAR_SIZE = 'w-6 h-6';
+  describe('icon title', () => {
+    it('null title should have aria-hidden attribute', () => {
+      const { getByTestId } = render(<TestIcon />);
+      expect(getByTestId('Icon')).toHaveAttribute('aria-hidden', 'true');
+    });
 
-  //   it('should be square', () => {
-  //     const { getByTestId } = render(<TestIcon />);
-  //     expect(getByTestId('Icon')).toHaveClass(REGULAR_SIZE);
-  //   });
+    it('role attribute should be `img`', () => {
+      const title = 'foo';
+      const { getByTestId } = render(<TestIcon title={title} />);
+      const icon = getByTestId('Icon');
+      expect(icon).toHaveAttribute('role', 'img');
+      expect(icon).not.toHaveAttribute('aria-hidden');
+    });
 
-  //   it('size auto should fill the available space', () => {
-  //     const { getByTestId } = render(<TestIcon auto />);
-  //     expect(getByTestId('Icon')).not.toHaveClass(REGULAR_SIZE);
-  //   });
+    it('should render a title element', () => {
+      const title = 'foo';
+      const { getByTitle } = render(<TestIcon title={title} />);
+      expect(getByTitle(title)).toHaveTextContent(title);
+    });
+  });
 
-  //   it('size can be small', () => {
-  //     const { getByTestId } = render(<TestIcon small />);
-  //     expect(getByTestId('Icon')).toHaveClass('w-5 h-5');
-  //   });
-  // });
-
-  // describe('icon title', () => {
-  //   it('null title should have aria-hidden attribute', () => {
-  //     const { getByTestId } = render(<TestIcon />);
-  //     expect(getByTestId('Icon')).toHaveAttribute('aria-hidden', 'true');
-  //   });
-
-  //   it('role attribute should be `img`', () => {
-  //     const title = 'foo';
-  //     const { getByTestId } = render(<TestIcon title={title} />);
-  //     const icon = getByTestId('Icon');
-  //     expect(icon).toHaveAttribute('role', 'img');
-  //     expect(icon).not.toHaveAttribute('aria-hidden');
-  //   });
-
-  //   it('should render a title element', () => {
-  //     const title = 'foo';
-  //     const { getByTitle } = render(<TestIcon title={title} />);
-  //     expect(getByTitle(title)).toHaveTextContent(title);
-  //   });
-  // });
-
-  // describe('all icons', () => {
-  //   test.each(Object.entries(icons))(
-  //     '%s icon renders correctly',
-  //     (icon, IconComp) => {
-  //       const { getByTestId } = render(<IconComp data-testid={icon} />);
-  //       expect(getByTestId(icon)).toMatchSnapshot();
-  //     },
-  //   );
-  // });
+  describe('all icons', () => {
+    test.each(Object.entries(icons))(
+      '%s icon renders correctly',
+      (icon, IconComp) => {
+        const { getByTestId } = render(<IconComp data-testid={icon} />);
+        expect(getByTestId(icon)).toMatchSnapshot();
+      },
+    );
+  });
 });
