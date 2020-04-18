e7b3fa1e3824200ed603de41188a5f7aa80e45a9
salchichongallo <salchichongallo1997@gmail.com>
2020-04-18 17:51:46 -0500

    refactor(SiteTemplate): use new header design

--- src/ui/templates/SiteTemplate.js
+++ src/ui/templates/SiteTemplate.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { SiteHeader } from 'ui/components/SiteHeader';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+
+import AppLogo from 'ui/components/AppLogo';
+import SearchBar from 'ui/components/SearchBar';
 import { ThreeRowTemplate } from 'ui/templates/ThreeRowTemplate';
 
+import HamburguerMenu, {
+  MenuItem,
+  MenuButton,
+  useHamburguer,
+} from 'ui/components/HamburguerMenu';
+
+const links = [
+  { text: 'Inicio', href: '/' },
+  { text: 'Buscar', href: '/buscar' },
+];
+
 export function SiteTemplate(props) {
+  const menu = useHamburguer();
+
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -13,13 +28,59 @@ export function SiteTemplate(props) {
     }
   };
 
+  const searchBar = (
+    <SearchBar onChange={handleSearch} placeholder="Buscar beca" />
+  );
+
   return (
     <ThreeRowTemplate
       header={
-        <SiteHeader
-          onSearch={handleSearch}
-          isInitiallySearching={location.state?.isSearching}
-        />
+        <div className="p-4 lg:pt-10 pb-0 max-w-screen-xl mx-auto mb-8 sm:mb-12">
+          <header data-testid="AppHeader">
+            <div className="flex items-center justify-between">
+              <AppLogo />
+
+              <div className="flex-1 flex items-center justify-end">
+                <div className="flex-1 flex items-center justify-center">
+                  <div className="hidden md:block mx-8 w-full max-w-lg">
+                    {searchBar}
+                  </div>
+                </div>
+
+                <nav className="hidden sm:block">
+                  {links.map(link => (
+                    <Link
+                      to={link.href}
+                      key={link.href}
+                      className="py-4 px-6 hover:underline"
+                    >
+                      {link.text}
+                    </Link>
+                  ))}
+                </nav>
+
+                <MenuButton
+                  className="sm:hidden"
+                  {...menu.getToggleButtonProps()}
+                  data-testid="AppHeader__menuButton"
+                />
+              </div>
+            </div>
+
+            <HamburguerMenu
+              isOpen={menu.isOpen}
+              data-testid="AppHeader__hamburguer"
+            >
+              {links.map(link => (
+                <MenuItem to={link.href} key={link.href}>
+                  {link.text}
+                </MenuItem>
+              ))}
+            </HamburguerMenu>
+
+            <div className="md:hidden mt-6">{searchBar}</div>
+          </header>
+        </div>
       }
       {...props}
     />
--- src/ui/templates/ThreeRowTemplate.js
+++ src/ui/templates/ThreeRowTemplate.js
@@ -5,9 +5,10 @@ import { AppFooter } from 'ui/components/AppFooter';
 export function ThreeRowTemplate({ header, className = '', ...restProps }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <div className="h-1 w-full bg-primary" />
       <div>{header}</div>
-      <div {...restProps} className={`h-full flex-1 ${className}`} />
-      <div>
+      <div {...restProps} className={`h-full flex-1 ${className} px-3`} />
+      <div className="mt-8">
         <AppFooter />
       </div>
     </div>
