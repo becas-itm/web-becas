a3d1dee3eb65ff69c0e90ec8a279eb5500d91ddd
salchichongallo <salchichongallo1997@gmail.com>
2020-04-18 15:33:38 -0500

    refactor(HomePage): use app search bar

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,103 +1,56 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-import { useToggle } from 'utils/hooks';
-import AppLogo from 'ui/components/AppLogo';
-import { Menu, Github } from 'ui/components/Icon';
-import IconButton from 'ui/components/IconButton';
+import { ArrowRightAlt } from 'ui/components/Icon';
+import AppHeader from 'ui/components/AppHeader/AppHeader';
+import SearchBar, { SearchBarButton } from 'ui/components/SearchBar';
 
-import { SearchBar } from './SearchBar';
 import { ReactComponent as ImgHero } from './imgResearching.svg';
-import './HomePage.scss';
 
 export default function HomePage() {
-  const [showMenu, toggleMenu] = useToggle();
+  const navigate = useNavigate();
+
+  const handleChange = term => {
+    navigate(`/buscar?term=${term}`, { state: { isSearching: true } });
+  };
+
   return (
-    <div className="pHome">
-      <header className="pHome__header pHome__container">
-        <AppLogo />
-        <div>
-          <nav className="pHome__menuLinks">
-            <Link
-              to="/buscar"
-              className="p-4 rounded hover:underline focus:underline"
-            >
-              Buscar
-            </Link>
-            <Link
-              to="/terminos"
-              className="p-4 mx-4 rounded hover:underline focus:underline"
-            >
-              Términos
-            </Link>
-            <Link
-              to="#"
-              className="p-4 rounded hover:underline focus:underline"
-            >
-              Nosotros
-            </Link>
-          </nav>
-          <div className="pHome__menuBtn">
-            <IconButton onClick={toggleMenu} large icon={Menu}>
-              Menú
-            </IconButton>
-          </div>
-        </div>
-      </header>
+    <div>
+      <div className="h-1 w-full bg-primary" />
+      <div className="p-4 lg:pt-10 pb-0 max-w-screen-xl mx-auto">
+        <AppHeader />
 
-      {showMenu && (
-        <nav className="bg-red mt-2 flex flex-col border-b">
-          <Link
-            to="/buscar"
-            className="p-4 rounded hover:underline focus:underline"
-          >
-            Buscar
-          </Link>
-          <Link
-            to="/terminos"
-            className="p-4 rounded hover:underline focus:underline"
-          >
-            Términos
-          </Link>
-          <Link to="#" className="p-4 rounded hover:underline focus:underline">
-            Nosotros
-          </Link>
-        </nav>
-      )}
-
-      <div className="pHome__container h-full flex-1">
-        <main className="pHome__main">
-          <div className="pHome__content">
-            <h1 className="pHome__title">
+        <div className="max-w-md mx-auto mt-6 lg:mt-12 lg:max-w-none flex flex-wrap justify-around">
+          <main className="mb-4 max-w-md">
+            <p className="font-mont font-semibold text-4xl sm:text-5xl leading-none mb-6">
               Encuentra nuevas oportunidades en el exterior
-            </h1>
+            </p>
 
-            <SearchBar />
+            <SearchBar
+              isLarge
+              clearable={false}
+              placeholder="Busca tu beca"
+              onChange={handleChange}
+              endIcon={
+                <SearchBarButton icon={ArrowRightAlt} type="submit">
+                  Buscar
+                </SearchBarButton>
+              }
+            />
 
-            <div className="pHome__writeUs">
+            <div className="mt-6">
               ¿Conoces alguna beca?{' '}
-              <Link to="#" className="underline py-4">
+              <Link to="#" className="underline hover:text-primary">
                 ¡Compártela!
               </Link>
             </div>
+          </main>
+
+          <div className="w-full max-w-md">
+            <ImgHero aria-hidden className="w-full h-full" />
           </div>
-
-          <ImgHero aria-hidden className="pHome__image w-full h-full" />
-        </main>
+        </div>
       </div>
-
-      <footer className="pHome__footer pHome__container">
-        <a
-          title="Ver becas en GitHub"
-          aria-label="Ver becas en GitHub"
-          href="https://github.com/orgs/becas-itm"
-          className="pHome__gitHub"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-        </a>
-      </footer>
     </div>
   );
 }
