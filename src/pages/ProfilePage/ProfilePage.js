321849a804fa6ebc8cbbf3c434291dc16f0e9979
salchichongallo <salchichongallo1997@gmail.com>
2020-04-20 09:29:20 -0500

    refactor(ProfilePage): extract profile card into component

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import * as yup from 'yup';
-import { Formik, Form } from 'formik';
-
 import { useAuth } from 'auth/index';
-
-import Input from 'ui/components/Input';
-import { FastField } from 'ui/components/formik';
-import Button, { COLOR } from 'ui/components/Button';
-import { Title, Actions } from 'ui/components/Dialog';
 import AdminTemplate from 'ui/templates/AdminTemplate';
-import GenderAvatarPicker from 'ui/components/GenderAvatarPicker';
 
 import { useEditUser } from './useEditUser';
-
-const validationSchema = yup.object().shape({
-  displayName: yup
-    .string()
-    .max(25, 'El nombre debe ser menor o igual a 25 carácteres')
-    .required('Ingresa tu nombre'),
-  password: yup
-    .string()
-    .min(6, 'Ingresa como mínimo 6 carácteres')
-    .max(100, 'La contraseña debe ser menor o igual a 100 carácteres'),
-});
+import { UserProfileCard } from './UserProfileCard';
 
 export default function ProfilePage() {
   const { user } = useAuth();
@@ -30,71 +11,8 @@ export default function ProfilePage() {
 
   return (
     <AdminTemplate>
-      <div className="w-full max-w-sm mx-auto bg-white shadow p-8 rounded mt-8">
-        <Title id="edit-profile">Perfil</Title>
-
-        <Formik
-          onSubmit={edit}
-          validationSchema={validationSchema}
-          initialValues={{
-            password: '',
-            gender: user.gender || '',
-            displayName: user.displayName || '',
-          }}
-        >
-          {({ values, setFieldValue }) => (
-            <Form noValidate>
-              <div className="block mb-4">
-                <div className="block text-base mb-1">Avatar</div>
-                <GenderAvatarPicker
-                  gender={values.gender}
-                  onGender={gender => setFieldValue('gender', gender)}
-                />
-              </div>
-
-              <div className="mb-4">
-                <FastField label="Nombre" name="displayName">
-                  <Input disabled={isLoading} placeholder="John Doe" />
-                </FastField>
-              </div>
-
-              <div className="block mb-4 opacity-50 cursor-not-allowed">
-                <FastField label="Correo electrónico" name="email">
-                  <Input
-                    readOnly
-                    disabled
-                    type="email"
-                    defaultValue={user.email || ''}
-                    className="cursor-not-allowed"
-                  />
-                </FastField>
-              </div>
-
-              <div className="mb-4">
-                <FastField label="Contraseña" name="password">
-                  <Input
-                    disabled={isLoading}
-                    type="password"
-                    placeholder="Sin cambiar"
-                  />
-                </FastField>
-              </div>
-
-              <Actions className="mt-6">
-                <Button
-                  color={COLOR.secondary}
-                  disabled={isLoading}
-                  type="reset"
-                >
-                  Restablecer
-                </Button>
-                <Button isLoading={isLoading} type="submit">
-                  Actualizar
-                </Button>
-              </Actions>
-            </Form>
-          )}
-        </Formik>
+      <div className="max-w-sm mx-auto my-8 mx-4">
+        <UserProfileCard user={user} onEdit={edit} isLoading={isLoading} />
       </div>
     </AdminTemplate>
   );
---
+++ src/pages/ProfilePage/UserProfileCard.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import * as yup from 'yup';
+import { Formik, Form, FastField as BaseFastField } from 'formik';
+
+import Input from 'ui/components/Input';
+import { FastField } from 'ui/components/formik';
+import Button, { COLOR } from 'ui/components/Button';
+import { Title, Actions } from 'ui/components/Dialog';
+import GenderAvatarPicker from 'ui/components/GenderAvatarPicker';
+
+const validationSchema = yup.object().shape({
+  displayName: yup
+    .string()
+    .max(25, 'El nombre debe ser menor o igual a 25 carácteres')
+    .required('Ingresa tu nombre'),
+  password: yup
+    .string()
+    .min(6, 'Ingresa como mínimo 6 carácteres')
+    .max(100, 'La contraseña debe ser menor o igual a 100 carácteres'),
+});
+
+export function UserProfileCard({ user, onEdit, isLoading }) {
+  return (
+    <div className="bg-white shadow p-8 rounded">
+      <Title>Perfil</Title>
+
+      <Formik
+        onSubmit={onEdit}
+        validationSchema={validationSchema}
+        initialValues={{
+          password: '',
+          gender: user.gender || '',
+          displayName: user.displayName || '',
+        }}
+      >
+        <Form noValidate>
+          <div className="block mb-4">
+            <div className="block text-base mb-1">Avatar</div>
+            <BaseFastField name="gender">
+              {({ field, form }) => (
+                <GenderAvatarPicker
+                  gender={field.value}
+                  onGender={gender => form.setFieldValue(field.name, gender)}
+                />
+              )}
+            </BaseFastField>
+          </div>
+
+          <div className="mb-4">
+            <FastField label="Nombre" name="displayName">
+              <Input disabled={isLoading} placeholder="John Doe" />
+            </FastField>
+          </div>
+
+          <div className="block mb-4 opacity-50 cursor-not-allowed">
+            <FastField label="Correo electrónico" name="email">
+              <Input
+                readOnly
+                disabled
+                type="email"
+                defaultValue={user.email || ''}
+                className="cursor-not-allowed"
+              />
+            </FastField>
+          </div>
+
+          <div className="mb-4">
+            <FastField label="Contraseña" name="password">
+              <Input
+                disabled={isLoading}
+                type="password"
+                placeholder="Sin cambiar"
+              />
+            </FastField>
+          </div>
+
+          <Actions className="mt-6">
+            <Button color={COLOR.secondary} disabled={isLoading} type="reset">
+              Restablecer
+            </Button>
+            <Button isLoading={isLoading} type="submit">
+              Actualizar
+            </Button>
+          </Actions>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
