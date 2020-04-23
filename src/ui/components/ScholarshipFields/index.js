967830123ffda57e4f6c14fccd53833ec61345f4
salchichongallo <salchichongallo1997@gmail.com>
2020-04-22 20:11:24 -0500

    feat(ScholarshipFields): export fields from index

---
+++ src/ui/components/ScholarshipFields/index.js
@@ -0,0 +1,15 @@
+export * from './AcademicLevel';
+
+export * from './Country';
+
+export * from './Deadline';
+
+export * from './Description';
+
+export * from './Entity';
+
+export * from './FundingType';
+
+export * from './Language';
+
+export * from './Name';
