94fd8ec00859b99d3ada8063b0b50d38bb7ad315
salchichongallo <salchichongallo1997@gmail.com>
2020-04-23 18:55:10 -0500

    feat(PrivateRoute): show snackbar when unauthenticated

--- src/ui/components/PrivateRoute.js
+++ src/ui/components/PrivateRoute.js
@@ -3,11 +3,14 @@ import { Route, Navigate } from 'react-router-dom';
 
 import { useAuth } from 'auth/useAuth';
 import { LOGIN_PATH } from 'auth/constants';
+import { useSnackbar } from 'ui/components/Snackbar';
 
 function PrivateRoute(props) {
   const { user } = useAuth();
+  const snack = useSnackbar();
 
   if (!user) {
+    snack.show('Inicia sesión para continuar.');
     return <Navigate to={LOGIN_PATH} replace />;
   }
 
