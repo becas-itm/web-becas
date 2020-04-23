c2d31d3c89d65fa3f04d2dfabf04bdf8eeb3be7b
salchichongallo <salchichongallo1997@gmail.com>
2020-04-22 21:26:03 -0500

    feat(DeadlineEditable): normalize value to fulfil HTML 5 input type

--- src/ui/components/ScholarshipFields/Deadline/DeadlineEditable.js
+++ src/ui/components/ScholarshipFields/Deadline/DeadlineEditable.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { format } from 'date-fns';
 import Input from 'ui/components/Input';
+
 import { Deadline } from './Deadline';
+
+const normalizeDate = date => format(new Date(date), 'yyyy-MM-dd');
 
 export function DeadlineEditable({ value, onChange }) {
   const handleChange = event => onChange(event.target.value);
   return (
     <Deadline>
       <Input
-        value={value}
+        value={value ? normalizeDate(value) : ''}
         onChange={handleChange}
         className="mt-2"
         type="date"
