d8c4c6a61691758ede3bbf74aa85b78e765e27b6
salchichongallo <salchichongallo1997@gmail.com>
2020-04-23 11:55:59 -0500

    feat(PendingScholarshipDetailPage): use go back button

--- src/pages/PendingScholarshipDetailPage/PendingScholarshipDetailPage.js
+++ src/pages/PendingScholarshipDetailPage/PendingScholarshipDetailPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 
 import { useToggle } from 'utils/hooks';
+import { Edit } from 'ui/components/Icon';
 import Spinner from 'ui/components/Spinner';
 import IconButton from 'ui/components/IconButton';
-import { ArrowBack, Edit } from 'ui/components/Icon';
+import GoBackButton from 'ui/components/GoBackButton';
 import AdminTemplate from 'ui/templates/AdminTemplate';
 
 import { useScholarship } from './useScholarship';
@@ -14,7 +14,6 @@ import EditableScholarship from './EditableScholarship';
 
 function PendingScholarshipDetailPage() {
   const { id } = useParams();
-  const navigate = useNavigate();
   const { scholarship, isFetching, refetch } = useScholarship(id);
 
   const [isEditing, toggleEdit] = useToggle();
@@ -27,9 +26,7 @@ function PendingScholarshipDetailPage() {
   return (
     <AdminTemplate>
       <header className="flex items-center md:hidden h-12 px-2 border-b bg-white">
-        <IconButton icon={ArrowBack} onClick={() => navigate(-1)}>
-          Atrás
-        </IconButton>
+        <GoBackButton />
         <h1 className="flex-1 mx-2">Convocatoria</h1>
         {!isEditing && (
           <IconButton onClick={toggleEdit} icon={Edit}>
