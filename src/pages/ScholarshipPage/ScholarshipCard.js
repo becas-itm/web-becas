ec3be266cbae7f7aa0adf6a8cb970a877866a76b
salchichongallo <salchichongallo1997@gmail.com>
2020-04-22 20:11:48 -0500

    refactor(ScholarshipPage): use new scholarshipo fields components
    use suspense to fetch data

---
+++ src/pages/ScholarshipPage/ScholarshipCard.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import GoBackButton from 'ui/components/GoBackButton';
+
+import {
+  Entity,
+  Country,
+  Deadline,
+  FundingType,
+  Description,
+  AcademicLevel,
+} from 'ui/components/ScholarshipFields';
+
+import { ScholarshipDetails } from './ScholarshipDetails';
+
+export function ScholarshipCard({
+  name,
+  description,
+  deadline,
+  academicLevel,
+  fundingType,
+  country,
+  entity,
+  sourceDetails,
+}) {
+  return (
+    <main className="max-w-screen-md mx-auto p-6 bg-white shadow rounded sm:py-10 md:py-12">
+      <div className="max-w-xl mx-auto">
+        <div className="mb-3 flex items-center">
+          <GoBackButton />
+          <div className="pl-2">Convocatoria</div>
+        </div>
+
+        <h1 className="text-xl sm:text-2xl font-semibold mb-3">{name}</h1>
+
+        <div className="mt-4">
+          <Description value={description} />
+        </div>
+
+        <div className="pl-8 mt-6">
+          <Deadline value={deadline} />
+
+          <div className="mt-4">
+            <AcademicLevel value={academicLevel} />
+          </div>
+
+          <div className="mt-4">
+            <FundingType value={fundingType} />
+          </div>
+
+          <div className="mt-4">
+            <Country {...(country || {})} />
+          </div>
+
+          <div className="flex -ml-8 mt-4">
+            <Entity name={entity.name} fullName={entity.fullName} />
+          </div>
+        </div>
+
+        <ScholarshipDetails {...sourceDetails} entityName={entity.name} />
+      </div>
+    </main>
+  );
+}
--- src/pages/ScholarshipPage/ScholarshipPage.js
+++ src/pages/ScholarshipPage/ScholarshipPage.js
@@ -1,128 +1,44 @@
 import React from 'react';
-import { get } from 'utils/api';
-import { useQuery } from 'react-query';
-import { useParams, useNavigate } from 'react-router';
+import { useGet } from 'utils/hooks';
+import { useParams } from 'react-router';
 
 import Spinner from 'ui/components/Spinner';
 import LinkButton from 'ui/components/LinkButton';
-import IconButton from 'ui/components/IconButton';
-import CountryFlag from 'ui/components/CountryFlag';
-import EntityAvatar from 'ui/components/EntityAvatar';
 import NotFoundGhost from 'ui/components/NotFoundGhost';
 import { SiteTemplate } from 'ui/templates/SiteTemplate';
-import { Event, School, Money, ArrowBack } from 'ui/components/Icon';
 
-import {
-  formatDeadline,
-  getFundingType,
-  getAcademicLevel,
-} from 'utils/scholarship';
-
-import { ScholarshipDetails } from './ScholarshipDetails';
+import { ScholarshipCard } from './ScholarshipCard';
 
 function ScholarshipPage() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const { data, isFetching } = useQuery(`/api/search/scholarships/${id}/`, get);
-
-  if (isFetching) {
-    return (
-      <SiteTemplate>
-        <div className="text-center">
-          <Spinner />
-        </div>
-      </SiteTemplate>
-    );
-  }
-
-  if (!data) {
-    return (
-      <SiteTemplate>
-        <NotFoundGhost
-          title="¡Oops!"
-          description="No pudimos encontrar la beca"
-        >
-          <LinkButton to="/buscar">Ir a página de búsqueda</LinkButton>
-        </NotFoundGhost>
-      </SiteTemplate>
-    );
-  }
+  const { id: scholarshipId } = useParams();
 
   return (
     <SiteTemplate>
-      <main className="max-w-screen-md mx-auto p-6 bg-white shadow rounded sm:py-10 md:py-12">
-        <div className="max-w-xl mx-auto">
-          <div className="mb-3 flex items-center">
-            <IconButton onClick={() => navigate(-1)} icon={ArrowBack}>
-              Ir atrás
-            </IconButton>
-            <div className="pl-2">Convocatoria</div>
+      <React.Suspense
+        fallback={
+          <div className="text-center">
+            <Spinner />
           </div>
-
-          <h1 className="text-xl sm:text-2xl font-semibold mb-3">
-            {data.name}
-          </h1>
-          <div className="text-sm text-medium">Descripción</div>
-          <p className="text-base">{data.description}</p>
-
-          <div className="pl-8 mt-6">
-            {data.deadline && (
-              <div className="flex">
-                <Event className="text-disabled mr-2" />
-                <div>
-                  <div className="text-sm text-medium">Abierta hasta</div>
-                  <div>{formatDeadline(data.deadline)}</div>
-                </div>
-              </div>
-            )}
-
-            <div className="flex mt-4">
-              <School className="text-disabled mr-2" />
-              <div>
-                <div className="text-sm text-medium">Tipo de Beca</div>
-                <div>{getAcademicLevel(data.academicLevel)}</div>
-              </div>
-            </div>
-
-            <div className="flex mt-4">
-              <Money className="text-disabled mr-2" />
-              <div>
-                <div className="text-sm text-medium">Financiamiento</div>
-                <div>{getFundingType(data.fundingType)}</div>
-              </div>
-            </div>
-
-            <div className="flex mt-4">
-              <CountryFlag
-                code={data.country.code}
-                style={{ width: 24, height: 24 }}
-                className="shadow-outline rounded-full mr-2 object-cover"
-              />
-              <div>
-                <div className="text-sm text-medium">País</div>
-                <div>{data.country.name}</div>
-              </div>
-            </div>
-
-            <div className="flex mt-4 -ml-8">
-              <div className="text-disabled mr-4">
-                <EntityAvatar name={data.entity.name} />
-              </div>
-              <div>
-                <div className="text-sm text-medium">Publicada por</div>
-                <div>{data.entity.fullName}</div>
-              </div>
-            </div>
-          </div>
-
-          <ScholarshipDetails
-            {...data.sourceDetails}
-            entityName={data.entity.name}
-          />
-        </div>
-      </main>
+        }
+      >
+        <PageFetcher scholarshipId={scholarshipId} />
+      </React.Suspense>
     </SiteTemplate>
   );
+}
+
+function PageFetcher({ scholarshipId }) {
+  const { data } = useGet(`/api/search/scholarships/${scholarshipId}/`);
+
+  if (!data) {
+    return (
+      <NotFoundGhost title="¡Oops!" description="No pudimos encontrar la beca">
+        <LinkButton to="/buscar">Ir a página de búsqueda</LinkButton>
+      </NotFoundGhost>
+    );
+  }
+
+  return <ScholarshipCard {...data} />;
 }
 
 export default ScholarshipPage;
