17b62910a0c5f6b4f9b8bb4802a29963ef23a1d4
salchichongallo <salchichongallo1997@gmail.com>
2020-04-22 21:24:44 -0500

    feat(DescriptionEditable): pass rest props

--- src/ui/components/ScholarshipFields/Description/DescriptionEditable.js
+++ src/ui/components/ScholarshipFields/Description/DescriptionEditable.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Textarea from 'ui/components/Textarea';
 import { Description } from './Description';
 
-export function DescriptionEditable({ value, onChange }) {
+export function DescriptionEditable({ value, onChange, ...restProps }) {
   const handleChange = event => onChange(event.target.value);
   return (
     <Description>
-      <Textarea value={value} onChange={handleChange} />
+      <Textarea {...restProps} value={value} onChange={handleChange} />
     </Description>
   );
 }
