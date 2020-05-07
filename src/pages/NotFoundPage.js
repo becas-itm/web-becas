257c3417c332cc81ef50f638227a092f7bfac94c
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-05-07 12:54:58 -0500

    refactor(NotFoundPage): use kawai ghost

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import LinkButton from 'ui/components/LinkButton';
+import KawaiGhost from 'ui/components/KawaiGhost';
 import { SiteTemplate } from 'ui/templates/SiteTemplate';
-import NotFoundGhost from 'ui/components/NotFoundGhost';
 
 export default function NotFoundPage() {
   return (
     <SiteTemplate>
-      <NotFoundGhost title="404" description="No pudimos encontrar la página">
-        <LinkButton to="/">Regresa al inicio</LinkButton>
-      </NotFoundGhost>
+      <main className="flex flex-col items-center text-center px-4 mt-8">
+        <KawaiGhost />
+        <h1 className="font-mont text-5xl mt-6">404</h1>
+        <p className="text-medium text-xl">No pudimos encontrar la página</p>
+        <LinkButton to="/" className="mt-4">
+          Regresa al inicio
+        </LinkButton>
+      </main>
     </SiteTemplate>
   );
 }
---
+++ src/ui/components/KawaiGhost/KawaiGhost.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ghostShocked from './ghostShocked.svg';
+
+function KawaiGhost() {
+  return <img src={ghostShocked} className="max-w-sm" alt="" aria-hidden />;
+}
+
+export default KawaiGhost;
