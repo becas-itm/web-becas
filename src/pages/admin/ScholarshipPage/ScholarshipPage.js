47d2d29915258c9b8f39780141d5894df51ec2a4
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-05-12 16:36:58 -0500

    feat(ScholarshipPage): add scholarship details steps

--- src/pages/admin/ScholarshipPage/ScholarshipPage.js
+++ src/pages/admin/ScholarshipPage/ScholarshipPage.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { useGet, useToggle } from 'utils/hooks';
 import GoBackButton from 'ui/components/GoBackButton';
 import AdminTemplate from 'ui/templates/AdminTemplate';
+import { ScholarshipDetails } from 'pages/ScholarshipPage/ScholarshipDetails';
 
 import { DenyDialog } from './PublishingSection/DenyDialog';
 import { BasicInfoSection } from './BasicInfoSection/BasicInfoSection';
@@ -39,13 +40,28 @@ function ScholarshipPage({ scholarship: initialScholarship }) {
       {({ values: scholarship }) => (
         <Form noValidate>
           <div className="mx-auto max-w-4xl pb-8">
-            <header className="flex items-center my-3 px-4 lg:px-0">
+            <header className="flex items-center my-2 lg:my-4 px-2 lg:px-0">
               <GoBackButton data-testid="goBack" />
-              <h1 className="text-xl pl-4">Convocatoria</h1>
+              <h1 className="text-xl pl-2">Convocatoria</h1>
             </header>
 
             <main>
               <BasicInfoSection {...scholarship} />
+
+              <section className="flex flex-wrap pt-2 px-4 lg:px-0">
+                <div className="mb-6 md:mb-0 md:flex-1" />
+
+                <div className="mt-6 w-full md:max-w-lg flex items-start">
+                  <div className="w-6 mr-3" />
+                  <div className="flex-1 flex items-start">
+                    <ScholarshipDetails
+                      {...scholarship.sourceDetails}
+                      entityName={scholarship.entity?.name}
+                    />
+                  </div>
+                </div>
+              </section>
+
               <LocalizationSection {...scholarship} />
               <PublishingSection
                 {...scholarship}
