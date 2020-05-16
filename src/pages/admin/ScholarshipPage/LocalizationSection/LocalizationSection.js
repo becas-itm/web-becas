a463f066171343e00143bfd4d5157b49ee79ed6c
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-05-15 19:41:53 -0500

    fix(ScholarshipPage): pass fieldsDisabled prop

--- src/pages/admin/ScholarshipPage/LocalizationSection/LocalizationSection.js
+++ src/pages/admin/ScholarshipPage/LocalizationSection/LocalizationSection.js
@@ -31,7 +31,7 @@ export function LocalizationSection({ country, fieldsDisabled = false }) {
             </div>
             <CountryCombobox
               name="country"
-              disabled={true}
+              disabled={fieldsDisabled}
               data-testid="country"
             />
           </div>
