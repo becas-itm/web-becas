1462201870b019bab2d5d4db0e6581451fa99f74
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-05-17 15:47:29 -0500

    refactor(ErrorPage): make title smaller

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -8,18 +8,20 @@ function ErrorPage() {
   const handleClick = () => window.location.reload();
   return (
     <div>
+      <div className="h-1 w-full bg-primary" />
+
       <div className="p-4 lg:pt-10 pb-0 max-w-screen-xl mx-auto">
         <AppLogo />
       </div>
 
       <main className="flex flex-col items-center text-center px-4 mt-12">
         <HeroImage
-          style={{ maxWidth: 296 }}
+          style={{ maxWidth: 248 }}
           className="w-full h-auto"
           aria-hidden
         />
-        <h1 className="font-mont text-5xl mt-6">Algo salió mal</h1>
-        <p className="text-medium text-xl">
+        <h1 className="font-mont text-4xl mt-6">Algo salió mal</h1>
+        <p className="text-medium text-xl mt-2">
           No te preocupes, estamos trabajando en ello.
         </p>
 
