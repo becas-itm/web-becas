c92469be72b54a9beea57461dca49615d324c2e3
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-06-06 16:23:09 -0500

    refactor(SplashScreen): use component as an alert

--- src/ui/SplashScreen.js
+++ src/ui/SplashScreen.js
@@ -4,9 +4,14 @@ import AppLogo from 'ui/AppLogo';
 
 function SplashScreen() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center bg-white">
+    <div
+      role="alert"
+      aria-live="polite"
+      className="w-screen h-screen flex flex-col items-center justify-center bg-white"
+    >
       <AppLogo />
       <Spinner className="mt-4" />
+      <div className="sr-only">Cargando...</div>
     </div>
   );
 }
