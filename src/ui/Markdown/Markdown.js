f26dba87371a01c36d17ee55534fc668606770f4
stivendiaz <stivendiazagudelo@gmail.com>
2020-06-07 22:12:25 -0500

    feat(ui): add link props to Markdown component

--- src/ui/Markdown/Markdown.js
+++ src/ui/Markdown/Markdown.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown/with-html';
-
 import './Markdown.scss';
 
-export default function Markdown({ src }) {
-  const ref = React.useRef();
-
-  React.useLayoutEffect(
-    function externalizeLinks() {
-      ref.current.querySelectorAll('a').forEach(anchor => {
-        anchor.setAttribute('target', '_blank');
-        anchor.setAttribute('rel', 'noopener noreferrer');
-      });
-    },
-    [src],
-  );
-
+function addLinkProps(props) {
   return (
-    <div ref={ref} className="Markdown">
-      <ReactMarkdown source={src} escapeHtml />
-    </div>
+    <a href={props.href} target="_blank" rel="noopener noreferrer">
+      {props.children}
+    </a>
+  );
+}
+
+export default function Markdown({ src }) {
+  return (
+    <ReactMarkdown
+      source={src}
+      className="Markdown"
+      escapeHtml
+      renderers={{
+        link: addLinkProps,
+      }}
+    />
   );
 }
