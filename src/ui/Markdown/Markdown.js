3a8aba9007a941845b4807ad9c118584594733bd
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-06-08 19:41:44 -0500

    feat(Markdown): externalize links

--- src/ui/Markdown/Markdown.js
+++ src/ui/Markdown/Markdown.js
@@ -4,5 +4,21 @@ import ReactMarkdown from 'react-markdown/with-html';
 import './Markdown.scss';
 
 export default function Markdown({ src }) {
-  return <ReactMarkdown source={src} escapeHtml className="Markdown" />;
+  const ref = React.useRef();
+
+  React.useLayoutEffect(
+    function externalizeLinks() {
+      ref.current.querySelectorAll('a').forEach(anchor => {
+        anchor.setAttribute('target', '_blank');
+        anchor.setAttribute('rel', 'noopener noreferrer');
+      });
+    },
+    [src],
+  );
+
+  return (
+    <div ref={ref} className="Markdown">
+      <ReactMarkdown source={src} escapeHtml />
+    </div>
+  );
 }
