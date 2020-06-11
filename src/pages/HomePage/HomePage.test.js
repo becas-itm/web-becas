3dcc09bd0e4a605a3bb644d7dd3cd81245b889ae
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-06-10 19:33:46 -0500

    test(HomePage): test page completely
    Add missing tests to landing page

--- src/pages/HomePage/HomePage.test.js
+++ src/pages/HomePage/HomePage.test.js
@@ -1,54 +1,72 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router';
-import { render, fireEvent } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, getByText } from '@testing-library/react';
 
-import { HomeHeader as BaseHomeHeader } from './HomeHeader';
+import HomePage from './HomePage';
 
-describe('Header', () => {
-  const HomeHeader = props => (
-    <MemoryRouter>
-      <BaseHomeHeader {...props} />
-    </MemoryRouter>
+function renderWithRouter() {
+  const history = createMemoryHistory();
+  const wrapper = ({ children }) => (
+    <Router history={history}>{children}</Router>
   );
+  return {
+    ...render(<HomePage />, { wrapper }),
+    history,
+  };
+}
 
-  test('renders correctly', () => {
-    const { queryByTestId } = render(<HomeHeader />);
-    const header = queryByTestId('HomeHeader');
-    expect(header).toBeInTheDocument();
-    expect(header.tagName).toBe('HEADER');
-  });
+test('renders correctly', () => {
+  renderWithRouter();
 
-  describe('navigation links', () => {
-    it('should have a link to Home Page', () => {
-      const { queryByText } = render(<HomeHeader />);
-      const item = queryByText('Inicio');
-      expect(item).toBeInTheDocument();
-      expect(item).toHaveAttribute('href', '/');
-    });
+  expect(
+    screen.queryByText('Encuentra nuevas oportunidades en el exterior'),
+  ).toBeInTheDocument();
+});
 
-    it('should have a link to Search Page', () => {
-      const { queryByText } = render(<HomeHeader />);
-      const item = queryByText('Buscar');
-      expect(item).toBeInTheDocument();
-      expect(item).toHaveAttribute('href', '/buscar');
-    });
-  });
+test('share scholarship link', () => {
+  const { history } = renderWithRouter();
+  const description = screen.getByText('¿Conoces alguna beca?');
 
-  describe('Navigation menu', () => {
-    const buttonId = 'HomeHeader__menuButton';
+  userEvent.click(getByText(description, '¡Compártela!'));
+  expect(history.location.pathname).toBe('/');
+});
 
-    it('should render a toggle button', () => {
-      const { queryByTestId } = render(<HomeHeader />);
-      expect(queryByTestId(buttonId)).toBeInTheDocument();
-    });
+test('search bar should redirect to search page', async () => {
+  const { history } = renderWithRouter();
 
-    it('should open navigation on click menu button', () => {
-      const { getByTestId, queryByTestId } = render(<HomeHeader />);
-      const button = getByTestId(buttonId);
+  const input = screen.getByPlaceholderText('Busca tu beca');
+  expect(input).toHaveValue('');
 
-      fireEvent.click(button);
+  const searchTerm = 'foo';
+  userEvent.type(input, searchTerm);
+  userEvent.click(screen.getByTestId('SearchBar__startButton'));
+  expect(history.location.pathname).toBe('/buscar');
 
-      expect(queryByTestId('HomeHeader__hamburger')).toBeInTheDocument();
-    });
+  const searchParams = new URLSearchParams(history.location.search);
+  expect(searchParams.has('term')).toBe(true);
+  expect(searchParams.get('term')).toBe(searchTerm);
+});
+
+test.each([
+  ['Inicio', '/'],
+  ['Buscar', '/buscar'],
+])('Header should have a link to `%s`', (text, href) => {
+  const { history } = renderWithRouter();
+  const header = screen.getByTestId('HomeHeader');
+  userEvent.click(getByText(header, text));
+  expect(history.location.pathname).toBe(href);
+});
+
+test('Navigation menu', () => {
+  renderWithRouter();
+  const menuButton = screen.getByTestId('HomeHeader__menuButton');
+
+  const hamburgerId = 'HomeHeader__hamburger';
+  expect(screen.queryByTestId(hamburgerId)).not.toBeInTheDocument();
+  userEvent.click(menuButton);
+  return waitFor(() => {
+    expect(screen.getByTestId(hamburgerId)).toBeInTheDocument();
   });
 });
