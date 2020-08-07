0ee8852265769f533c0dd12208e6c65ec689de33
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-08-07 11:37:04 -0500

    refactor(AdminTemplate): use new design

---
+++ src/admin/ui/AdminTemplate/AdminHeader.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import AppLogo from 'ui/AppLogo';
+import { useAuth } from 'auth/hooks';
+import UserActions from 'ui/UserActions';
+
+export function AdminHeader() {
+  const { user, logout } = useAuth();
+
+  return (
+    <header className="flex items-center flex-wrap">
+      <div className="lg:w-full max-w-xs">
+        <AppLogo>Admin</AppLogo>
+      </div>
+
+      <nav className="w-full lg:max-w-xs mt-2 lg:mt-0 order-last lg:order-none">
+        <ul className="flex flex-wrap text-center text-medium">
+          <li className="flex-1">
+            <NavLink
+              to="/pendientes"
+              className="block px-2 py-3 hover:text-primary"
+              activeClassName="font-semibold text-primary"
+            >
+              Becas
+            </NavLink>
+          </li>
+          <li className="flex-1">
+            <NavLink
+              to="/entidades"
+              className="block px-2 py-3 hover:text-primary"
+              activeClassName="font-semibold text-primary"
+            >
+              Entidades
+            </NavLink>
+          </li>
+          <li className="flex-1">
+            <NavLink
+              to="/usuarios"
+              className="block px-2 py-3 hover:text-primary"
+              activeClassName="font-semibold text-primary"
+            >
+              Usuarios
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="flex-1 flex justify-end">
+        <UserActions user={user} onLogout={logout} />
+      </div>
+    </header>
+  );
+}
--- src/admin/ui/AdminTemplate/AdminTemplate.js
+++ src/admin/ui/AdminTemplate/AdminTemplate.js
@@ -1,66 +1,39 @@
 import React from 'react';
-import { useAuth } from 'auth/hooks';
-import { useToggle } from 'utils/hooks';
 
-import AppLogo from 'ui/AppLogo';
+import Spinner from 'ui/Spinner';
 import AppFooter from 'ui/AppFooter';
-import IconButton from 'ui/IconButton';
-import UserActions from 'ui/UserActions';
-import NavRail, { NavItem } from 'ui/NavRail';
+import PageRibbon from 'ui/PageRibbon';
 
-import { Home, Inbox, SupervisorAccount, Menu, Business } from 'ui/Icon';
+import { AdminHeader } from './AdminHeader';
 
-import { MenuDrawer } from './MenuDrawer';
-import './AdminTemplate.css';
-
-const AdminTemplate = React.memo(function AdminTemplate({
-  children,
-  ...restProps
-}) {
-  const { user, logout } = useAuth();
-  const [showMenu, toggleMenu] = useToggle();
-
+const AdminTemplate = React.memo(function AdminTemplate({ children }) {
   return (
-    <div className="AdminTemplate">
-      <header className="bg-white border-b">
-        <div className="h-16 flex items-center justify-between mx-auto px-4">
-          <AppLogo>Admin</AppLogo>
-          <div>
-            <div className="hidden sm:block">
-              <UserActions user={user} onLogout={logout} />
-            </div>
-            <div className="sm:hidden">
-              <IconButton onClick={toggleMenu} icon={Menu}>
-                Abrir menú
-              </IconButton>
-            </div>
-          </div>
+    <>
+      <div
+        className="min-h-screen flex flex-col"
+        style={{ background: '#fafdff' }}
+      >
+        <PageRibbon />
+
+        <div className="w-full max-w-2xl lg:max-w-6xl mx-auto mt-2 lg:mt-8 px-4">
+          <AdminHeader />
         </div>
-      </header>
 
-      <NavRail>
-        <NavItem exact to="/" icon={Home}>
-          Inicio
-        </NavItem>
-        <NavItem to="/pendientes" icon={Inbox}>
-          Becas
-        </NavItem>
-        <NavItem to="/entidades" icon={Business}>
-          Entidades
-        </NavItem>
-        <NavItem to="/usuarios" icon={SupervisorAccount}>
-          Usuarios
-        </NavItem>
-      </NavRail>
-
-      <div {...restProps}>{children}</div>
+        <div className="flex-1">
+          <React.Suspense
+            fallback={
+              <div className="text-center mt-4">
+                <Spinner />
+              </div>
+            }
+          >
+            {children}
+          </React.Suspense>
+        </div>
+      </div>
 
       <AppFooter />
-
-      {showMenu && (
-        <MenuDrawer user={user} onDismiss={toggleMenu} onLogout={logout} />
-      )}
-    </div>
+    </>
   );
 });
 
